06be93846a7c690f33eba1301b78b9e53fe75789
kariikalan2013 <114467276+kariikalan2013@users.noreply.github.com>
2022-09-28 14:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu=document.querySelector(".menu-btn")
+
+let links=document.querySelector(".nav-links")
+menu.addEventListener('click',()=>{
+  links.classList.toggle("active");
+})  
+    
+
+
